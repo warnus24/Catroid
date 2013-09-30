f125a8a7497fa97b808abed37e28f15647459501
Michael Stradner <michael.stradner@student.tugraz.at>
2013-09-30 10:15:31 +0200

    Robot albert distance sensors implemented (in formula editor)

--- catroid/src/org/catrobat/catroid/formulaeditor/SensorHandler.java
+++ catroid/src/org/catrobat/catroid/formulaeditor/SensorHandler.java
@@ -43,6 +43,8 @@ public final class SensorHandler implements SensorEventListener, SensorCustomEve
 	private float linearAcceleartionZ = 0f;
 
 	private float loudness = 0f;
+	private float albertRobotDistanceLeft = 0f;
+	private float albertRobotDistanceRight = 0f;
 
 	private SensorHandler(Context context) {
 		sensorManager = new SensorManager(
@@ -63,6 +65,8 @@ public final class SensorHandler implements SensorEventListener, SensorCustomEve
 		instance.sensorManager.registerListener(instance, instance.rotationVectorSensor,
 				android.hardware.SensorManager.SENSOR_DELAY_NORMAL);
 		instance.sensorManager.registerListener(instance, Sensors.LOUDNESS);
+		instance.sensorManager.registerListener(instance, Sensors.ALBERT_ROBOT_DISTANCE_LEFT);
+		instance.sensorManager.registerListener(instance, Sensors.ALBERT_ROBOT_DISTANCE_RIGHT);
 	}
 
 	public static void registerListener(SensorEventListener listener) {
@@ -147,6 +151,10 @@ public final class SensorHandler implements SensorEventListener, SensorCustomEve
 
 			case LOUDNESS:
 				return Double.valueOf(instance.loudness);
+			case ALBERT_ROBOT_DISTANCE_LEFT:
+				return Double.valueOf(instance.albertRobotDistanceLeft);
+			case ALBERT_ROBOT_DISTANCE_RIGHT:
+				return Double.valueOf(instance.albertRobotDistanceRight);
 		}
 		return 0d;
 	}
@@ -178,6 +186,12 @@ public final class SensorHandler implements SensorEventListener, SensorCustomEve
 		switch (event.sensor) {
 			case LOUDNESS:
 				instance.loudness = event.values[0];
+				break;
+			case ALBERT_ROBOT_DISTANCE_LEFT:
+				instance.albertRobotDistanceLeft = event.values[0];
+				break;
+			case ALBERT_ROBOT_DISTANCE_RIGHT:
+				instance.albertRobotDistanceRight = event.values[1];
 				break;
 			default:
 				Log.v(TAG, "Unhandled sensor: " + event.sensor);
