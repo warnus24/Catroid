59014996b195bf019333927b9c76f221daf509e8
Michael Stradner <michi.strad@gmail.com>
2014-02-24 16:43:28 +0100

    testing

--- catroidTest/src/org/catrobat/catroid/uitest/stage/RobotAlbertTestSensor.java
+++ catroidTest/src/org/catrobat/catroid/uitest/stage/RobotAlbertTestSensor.java
@@ -57,7 +57,8 @@ public class RobotAlbertTestSensor extends BaseActivityInstrumentationTestCase<M
 	// needed for testdevices
 	// Bluetooth server is running with a name that starts with 'kitty'
 	// e.g. kittyroid-0, kittyslave-0
-	private static final String PAIRED_BLUETOOTH_SERVER_DEVICE_NAME = "T420";
+	//private static final String PAIRED_BLUETOOTH_SERVER_DEVICE_NAME = "T420";
+	private static final String PAIRED_BLUETOOTH_SERVER_DEVICE_NAME = "kittyslave";
 
 	private final String projectName = UiTestUtils.PROJECTNAME1;
 	private final String spriteName = "testSprite";
