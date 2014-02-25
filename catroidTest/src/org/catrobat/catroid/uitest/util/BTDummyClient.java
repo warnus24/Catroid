b1247c9f5514e36946b4a8f3c4ef5609072ee3fc
Michael Stradner <michi.strad@gmail.com>
2014-02-25 14:49:38 +0100

    testing

--- catroidTest/src/org/catrobat/catroid/uitest/util/BTDummyClient.java
+++ catroidTest/src/org/catrobat/catroid/uitest/util/BTDummyClient.java
@@ -161,6 +161,7 @@ public class BTDummyClient {
 
 			} catch (IOException e) {
 				Log.d("Multiplayer", "TestReceiver Thread END in Exeption");
+				Log.d("Multiplayer", e.getMessage());
 			}
 			Log.d("Multiplayer", "TestReceiver Thread END");
 		}
