fbda01ee28657c4f8b2aef2a9351d89adfdc5071
tafphil <TafPhil@gmail.com>
2014-03-11 00:41:00 +0100

    fix bugs -> negative friction, copy, load projects
    solve negative friction;
    copy objects is now possible,
    loading projects works

--- catroid/src/org/catrobat/catroid/ProjectManager.java
+++ catroid/src/org/catrobat/catroid/ProjectManager.java
@@ -388,7 +388,7 @@ public final class ProjectManager implements OnLoadProjectCompleteListener, OnCh
 	}
 
 	public FileChecksumContainer getFileChecksumContainer() {
-		return this.fileChecksumContainer;
+		return fileChecksumContainer;
 	}
 
 	public void setFileChecksumContainer(FileChecksumContainer fileChecksumContainer) {
