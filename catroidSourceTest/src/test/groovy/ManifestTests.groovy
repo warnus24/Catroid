6933608d9120cc2cdfb0e402235e8645e4f817e8
Roland Urbano <urbano@student.tugraz.at>
2014-07-02 21:52:18 +0200

    fixed ManifestTests. working now

---
+++ catroidSourceTest/src/test/groovy/ManifestTests.groovy
@@ -0,0 +1,46 @@
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+public class ManifestTests {
+    def manifestXML
+    def namespace
+
+    @Before void setUp() {
+        manifestXML = new XmlParser().parse(new File("catroid/build/intermediates/manifests/standalone/AndroidManifest.xml"))
+        namespace = new groovy.xml.Namespace('http://schemas.android.com/apk/res/android')
+    }
+
+    @After void tearDown() {
+        manifestXML = null;
+    }
+
+    @Test
+    public void testAppName() {
+        def codeXML = new XmlParser().parse(new File("catroid/src/main/assets/standalone/code.xml"))
+        println manifestXML.application[0].attribute(namespace.label) + ' vs ' + codeXML.header.programName.text()
+        assert manifestXML.application[0].attribute(namespace.label) == codeXML.header.programName.text()
+    }
+
+    @Test
+    public void testVersionName() {
+        println manifestXML.attribute(namespace.versionName)
+        assert manifestXML.attribute(namespace.versionName) == "0.9.-1.standalone"
+    }
+
+    @Test
+    public void testVersionCode() {
+        println manifestXML.attribute(namespace.versionCode)
+        assert manifestXML.attribute(namespace.versionCode) == "-1"
+    }
+
+    @Test
+    public void testPackageName() {
+        def codeXML = new XmlParser().parse(new File("src/main/assets/standalone/code.xml"))
+        def programName = codeXML.header.programName.text()
+        programName = programName.replaceAll(" ", "")
+        println manifestXML.attribute('package') + ' vs ' + ("org.catrobat.catroid." + programName)
+        assert manifestXML.attribute('package') == ("org.catrobat.catroid." + programName) //change this!
+    }
+}
