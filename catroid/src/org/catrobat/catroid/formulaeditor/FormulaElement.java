3c58a211aa385b1f158b03c433a5a3e13f09a1d3
bernhard <bernhardsp@gmail.com>
2014-07-28 10:14:17 +0200

    length of list implemented

--- catroid/src/org/catrobat/catroid/formulaeditor/FormulaElement.java
+++ catroid/src/org/catrobat/catroid/formulaeditor/FormulaElement.java
@@ -449,6 +449,9 @@ public class FormulaElement implements Serializable {
 		if (leftChild.type == ElementType.USER_VARIABLE) {
 			return (double) handleLengthUserVariableParameter(sprite);
 		}
+		if (leftChild.type == ElementType.USER_LIST) {
+			return (double) handleLengthUserListParameter(sprite);
+		}
 		if (left instanceof Double && ((Double) left).isNaN()) {
 			return 0d;
 		}
@@ -862,6 +865,17 @@ public class FormulaElement implements Serializable {
 				return Double.toString(((Double) userVariableValue)).length();
 			}
 		}
+
+	}private int handleLengthUserListParameter(Sprite sprite) {
+		UserListContainer userListContainer = ProjectManager.getInstance().getCurrentProject()
+				.getUserLists();
+		UserList userList = userListContainer.getUserList(leftChild.value, sprite);
+
+		if(userList == null){
+			return 0;
+		}
+
+		return userList.getList().size();
 
 	}
 
