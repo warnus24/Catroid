e4eb9d65779387c31e3771498f0a592d5b7ad997
Andreas Mueller <muel.andreas@gmail.com>
2014-08-04 12:36:53 +0200

    changed SpriteTest and removed unused imports

--- catroid/src/org/catrobat/catroid/content/bricks/UserScriptDefinitionBrick.java
+++ catroid/src/org/catrobat/catroid/content/bricks/UserScriptDefinitionBrick.java
@@ -28,7 +28,6 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.View.MeasureSpec;
--- catroid/src/org/catrobat/catroid/ui/fragment/AddBrickFragment.java
+++ catroid/src/org/catrobat/catroid/ui/fragment/AddBrickFragment.java
@@ -37,7 +37,6 @@ import android.support.v4.app.FragmentTransaction;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
