178df1b7ae51fc7421fdad7f550037b04017bea8
Franz Schreiner <grunztiger@gmail.com>
2014-08-14 15:59:08 +0200

    Note- and SpeakBrick use new Formula mechanics.
    IfLogicAction- and RepeatActionTest: testNullFormula() update.
    Fix of some Brickrefactoring related UI-tests.
    CatrobatLanguageVersion support. (Note- and SpeakBrick language conversion to FormulaBrick)
    Remove of redudant Null-Formula test method (SpeakStageTest.testNullText())

--- catroid/src/org/catrobat/catroid/ProjectManager.java
+++ catroid/src/org/catrobat/catroid/ProjectManager.java
@@ -144,11 +144,12 @@ public final class ProjectManager implements OnLoadProjectCompleteListener, OnCh
 				project.setScreenMode(ScreenModes.STRETCH);
 				checkNestingBrickReferences(false);
 			}
-//			if (project.getCatrobatLanguageVersion() == 0.92f || project.getCatrobatLanguageVersion() == 0.93f) {
-//				//0.93 should be left out because it available unintentional for a day
-//				//raise language version here to 0.94
-//			}
-			//insert further conversions here
+
+			if (project.getCatrobatLanguageVersion() == 0.92f) {
+				project.setCatrobatLanguageVersion(0.93f);
+			}
+
+			//insert further convertions here
 
 			checkNestingBrickReferences(true);
 			if (project.getCatrobatLanguageVersion() == Constants.CURRENT_CATROBAT_LANGUAGE_VERSION) {
