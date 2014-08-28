a6798dd175ac82b5d3670c606696b22d22a86735
Adrian Schnedlitz <a.schnedlitz@student.tugraz.at>
2014-08-28 08:06:29 +0200

    rebased branch on the actual master + fixed all bugs (Brick + Functions are running as usual again)

--- catroid/src/org/catrobat/catroid/stage/PreStageActivity.java
+++ catroid/src/org/catrobat/catroid/stage/PreStageActivity.java
@@ -304,7 +304,6 @@ public class PreStageActivity extends BaseActivity {
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		Log.i("bt", "requestcode " + requestCode + " result code" + resultCode);
-
 		switch (requestCode) {
 
 			case REQUEST_CONNECT_DEVICE:
