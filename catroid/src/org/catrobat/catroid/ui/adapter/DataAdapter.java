2520cf24e4e921509e02b121e864dc2597809291
bernhard <bernhardsp@gmail.com>
2014-08-29 18:52:38 +0200

    remove unused import

--- catroid/src/org/catrobat/catroid/ui/adapter/DataAdapter.java
+++ catroid/src/org/catrobat/catroid/ui/adapter/DataAdapter.java
@@ -26,7 +26,6 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
