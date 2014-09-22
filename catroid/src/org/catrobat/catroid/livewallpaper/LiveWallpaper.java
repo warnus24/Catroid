42e7ab8bd7bec55b819eee7c2c303046e8ae8adc
Patrik Maier <patrik.maier@student.tugraz.at>
2014-09-22 18:02:51 +0200

    Synchronisation Problems fixed

--- catroid/src/org/catrobat/catroid/livewallpaper/LiveWallpaper.java
+++ catroid/src/org/catrobat/catroid/livewallpaper/LiveWallpaper.java
@@ -404,13 +404,15 @@ public class LiveWallpaper extends AndroidLiveWallpaperService {
 			}
 			//onPause();
 			LiveWallpaperEngine engine = this;
-			getLocalStageListener().create();
-			getLocalStageListener().reloadProjectLWP(engine);
+
+
 			Log.d("LWP", "StageListener, changeWallpaper Engine: " + name);
 
 			synchronized (engine) {
 				try {
 					Log.d("LWP", "StageListener, changeWallpaper wait... ANFANG");
+					getLocalStageListener().create();
+					getLocalStageListener().reloadProjectLWP(engine);
 					onResume();
 					engine.wait();
 				} catch (InterruptedException e) {
