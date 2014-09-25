7d22ea9eb34eeb8198282989e4cb8a95178a03f6
Patrik Maier <patrik.maier@student.tugraz.at>
2014-09-25 17:56:05 +0200

    Add SelectPostProcessingEffectActivityTest

---
+++ catroid/src/org/catrobat/catroid/livewallpaper/postprocessing/PostProcessingUtil.java
@@ -0,0 +1,7 @@
+package org.catrobat.catroid.livewallpaper.postprocessing;
+
+/**
+ * Created by White on 25.09.2014.
+ */
+public class PostProcessingUtil {
+}
---
+++ catroidTest/src/org/catrobat/catroid/test/livewallpaper/SelectPostProcessingEffectActivityTest.java
@@ -0,0 +1,7 @@
+package org.catrobat.catroid.test.livewallpaper;
+
+/**
+ * Created by White on 25.09.2014.
+ */
+public class SelectPostProcessingEffectActivityTest {
+}
